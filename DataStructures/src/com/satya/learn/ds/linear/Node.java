aefac58618dd516e0db37a49f35c282a05e755ac
satyendrakumarsharma <703162557@BSEZ1F485D.IND.CORP.AD>
2017-06-15 13:29:24 +0530

    Added element comments with details about node.

--- DataStructures/src/com/satya/learn/ds/linear/Node.java
+++ DataStructures/src/com/satya/learn/ds/linear/Node.java
@@ -1,5 +1,13 @@
 package com.satya.learn.ds.linear;
 
+/**
+ * The node to represent each unit of a data-structure.
+ * 
+ * @author Satyendra
+ *
+ * @param <E>
+ *            type of data held by this node.
+ */
 public final class Node<E> {
 	E data;
 	Node<E> next;
