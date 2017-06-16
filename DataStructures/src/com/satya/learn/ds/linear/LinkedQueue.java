bcaae4f051596daed0b1b28e0ad060383deb52e6
satyendrakumarsharma <703162557@BSEZ1F485D.IND.CORP.AD>
2017-06-16 20:45:13 +0530

    Introduced a queue implemented with linked nodes.

---
+++ DataStructures/src/com/satya/learn/ds/linear/LinkedQueue.java
@@ -0,0 +1,60 @@
+package com.satya.learn.ds.linear;
+
+/**
+ * Implementation of queue using a linked list.<br/>
+ * 
+ * @author Satyendra
+ *
+ */
+public class LinkedQueue<E> implements Queue<E> {
+
+	/**
+	 * 
+	 */
+	public LinkedQueue() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean enQueue(E e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public E deQueue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E frontPeek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public E rearPeek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
